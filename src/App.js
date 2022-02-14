d12f45d241e04da13f941d1893785802166838d1
koreaaerospaceU <zx811@naver.com>
2022-02-15 02:15:29 +0900

    fix: 미사용 라우트 정리

--- src/App.js
+++ src/App.js
@@ -2,11 +2,8 @@ import './App.css';
 import React from 'react'
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Team from './pages/team'
-import NewPost from './pages/post/new'
 import NewTeam from './pages/new/team'
 import NewSubject from './pages/new/subject'
-import OldPost from './pages/post/old'
-import Board from './pages/board'
 import Login from './pages/login'
 import Setting from './pages/setting'
 import Subject from './pages/subject'
@@ -32,15 +29,6 @@ function App() {
         </Route>
         <Route exact path="/setting">
           <Setting />
-        </Route>
-        <Route exact path="/post/new">
-          <NewPost />
-        </Route>
-        <Route exact path="/post/:id">
-          <OldPost />
-        </Route>
-        <Route exact path="/board/:id">
-          <Board />
         </Route>
         <Redirect path="*" to="/setting" />
       </Switch>
